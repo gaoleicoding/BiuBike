d1615409227ab1e5d488be971cdf15da95ecdb56
GaoLei <gaolei20@guazi.com>
2020-01-05 18:15:20 +0800

    [add]1、加入骑行导航

---
+++ BiuBike/app/src/main/java/com/biubike/activity/BNaviGuideActivity.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package com.baidu.bikenavi_demo;
+
+import com.baidu.mapapi.bikenavi.BikeNavigateHelper;
+import com.baidu.mapapi.bikenavi.adapter.IBNaviStatusListener;
+import com.baidu.mapapi.bikenavi.adapter.IBRouteGuidanceListener;
+import com.baidu.mapapi.bikenavi.adapter.IBTTSPlayer;
+import com.baidu.mapapi.bikenavi.model.BikeRouteDetailInfo;
+import com.baidu.mapapi.bikenavi.params.BikeNaviLaunchParam;
+import com.baidu.mapapi.walknavi.model.RouteGuideKind;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class BNaviGuideActivity extends Activity {
+
+    private final static String TAG = BNaviGuideActivity.class.getSimpleName();
+
+    private BikeNavigateHelper mNaviHelper;
+
+    BikeNaviLaunchParam param;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mNaviHelper.quit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNaviHelper.resume();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mNaviHelper = BikeNavigateHelper.getInstance();
+
+        View view = mNaviHelper.onCreate(BNaviGuideActivity.this);
+        if (view != null) {
+            setContentView(view);
+        }
+
+        mNaviHelper.setBikeNaviStatusListener(new IBNaviStatusListener() {
+            @Override
+            public void onNaviExit() {
+                Log.d(TAG, "onNaviExit");
+            }
+        });
+
+        mNaviHelper.setTTsPlayer(new IBTTSPlayer() {
+            @Override
+            public int playTTSText(String s, boolean b) {
+                Log.d("tts", s);
+                return 0;
+            }
+        });
+
+        mNaviHelper.startBikeNavi(BNaviGuideActivity.this);
+
+        mNaviHelper.setRouteGuidanceListener(this, new IBRouteGuidanceListener() {
+            @Override
+            public void onRouteGuideIconUpdate(Drawable icon) {
+
+            }
+
+            @Override
+            public void onRouteGuideKind(RouteGuideKind routeGuideKind) {
+
+            }
+
+            @Override
+            public void onRoadGuideTextUpdate(CharSequence charSequence, CharSequence charSequence1) {
+
+            }
+
+            @Override
+            public void onRemainDistanceUpdate(CharSequence charSequence) {
+
+            }
+
+            @Override
+            public void onRemainTimeUpdate(CharSequence charSequence) {
+
+            }
+
+            @Override
+            public void onGpsStatusChange(CharSequence charSequence, Drawable drawable) {
+
+            }
+
+            @Override
+            public void onRouteFarAway(CharSequence charSequence, Drawable drawable) {
+
+            }
+
+            @Override
+            public void onRoutePlanYawing(CharSequence charSequence, Drawable drawable) {
+
+            }
+
+            @Override
+            public void onReRouteComplete() {
+
+            }
+
+            @Override
+            public void onArriveDest() {
+
+            }
+
+            @Override
+            public void onVibrate() {
+
+            }
+
+            @Override
+            public void onGetRouteDetailInfo(BikeRouteDetailInfo bikeRouteDetailInfo) {
+
+            }
+        });
+    }
+
+}
